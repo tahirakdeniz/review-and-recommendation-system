c53b73b1cd91124f61b1706af81b06c06e8fcbb8
IsmailUlasUnal <b2200356001@cs.hacettepe.edu.tr>
2024-04-12 13:00:36 +0300

    necessarry main class and pom.xml created
    necessary files for running app has been created

---
+++ backend/src/test/java/com/rrss/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrss.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
