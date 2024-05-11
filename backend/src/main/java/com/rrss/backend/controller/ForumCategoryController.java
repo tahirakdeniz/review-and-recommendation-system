56fee0c0202a2c7180d37825bc9b5f7662979a18
NurullahBaser <nurullahbaserr@gmail.com>
2024-05-11 20:41:10 +0300

    added read forum categories

--- backend/src/main/java/com/rrss/backend/controller/ForumCategoryController.java
+++ backend/src/main/java/com/rrss/backend/controller/ForumCategoryController.java
@@ -2,15 +2,14 @@ package com.rrss.backend.controller;
 
 import com.rrss.backend.dto.AddForumCategoryRequest;
 import com.rrss.backend.dto.ForumCategoryDto;
+import com.rrss.backend.dto.TopicDto;
 import com.rrss.backend.service.ForumCategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/forum-categories")
@@ -25,6 +24,11 @@ public class ForumCategoryController {
     @PostMapping
     public ResponseEntity<ForumCategoryDto> addForumCategory(@RequestBody AddForumCategoryRequest addForumCategoryRequest) {
         return new ResponseEntity<>(forumCategoryService.addForumCategory(addForumCategoryRequest), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ForumCategoryDto>> getCategories() {
+        return ResponseEntity.ok(forumCategoryService.getCategories());
     }
 
 }
