4cfcfded50a17aa5ca419a7260d0a9a3f137bb91
Muhammed Tahir Akdeniz <b2200356027@cs.hacettepe.edu.tr>
2024-05-14 16:35:28 +0300

    Shop page completed

--- frontend/src/app/(application)/shop/page.tsx
+++ frontend/src/app/(application)/shop/page.tsx
@@ -1,82 +1,9 @@
-'use client';
-import React, { useState } from 'react';
-import { Card, Carousel, Button, message } from 'antd';
-import { ShoppingCartOutlined } from '@ant-design/icons';
-import { useSearchParams } from 'next/navigation'
+import ShopClient from "@/components/ShopClient";
 
-// Mock data for demonstration purposes
-const exampleProducts = [
-    { id: '1', name: 'Coffee Maker', price: '99.99', image: 'https://cdn.pixabay.com/photo/2023/08/25/10/33/apples-8212695_1280.jpg' },
-    { id: '2', name: 'Espresso Machine', price: '189.99', image: 'https://cdn.pixabay.com/photo/2023/08/25/10/33/apples-8212695_1280.jpg' },
-    { id: '3', name: 'French Press', price: '29.99', image: 'https://cdn.pixabay.com/photo/2023/08/25/10/33/apples-8212695_1280.jpg' },
-    { id: '4', name: 'Coffee Grinder', price: '75.99', image: 'https://cdn.pixabay.com/photo/2023/08/25/10/33/apples-8212695_1280.jpg' },
-    { id: '5', name: 'Kettle', price: '43.99', image: 'https://cdn.pixabay.com/photo/2023/08/25/10/33/apples-8212695_1280.jpg' },
-    { id: '6', name: 'Tea Infuser', price: '13.99', image: 'https://cdn.pixabay.com/photo/2023/08/25/10/33/apples-8212695_1280.jpg' },
-    { id: '7', name: 'Travel Mug', price: '22.99', image: 'https://cdn.pixabay.com/photo/2023/08/25/10/33/apples-8212695_1280.jpg' },
-    { id: '8', name: 'Thermos', price: '19.99', image: 'https://cdn.pixabay.com/photo/2023/08/25/10/33/apples-8212695_1280.jpg' },
-    { id: '9', name: 'Ceramic Dripper', price: '17.99', image: 'https://cdn.pixabay.com/photo/2023/08/25/10/33/apples-8212695_1280.jpg' },
-    { id: '10', name: 'Filter Paper', price: '4.99', image: 'https://cdn.pixabay.com/photo/2023/08/25/10/33/apples-8212695_1280.jpg' }
-];
-
-interface Product {
-    id: string;
-    name: string;
-    price: string;
-    image?: string;
-}
-
-interface ProductListProps {
-    data: Product[];
-    title: string;
-}
-
-const ProductPage: React.FC = () => {
-    const [messageApi, contextHolder] = message.useMessage();
-    const searchParams = useSearchParams();
-
-    const search = searchParams.get('search');
-    const category = searchParams.get('category');
-    console.log(search, category)
-
-    // Using exampleProducts for all categories, just for display
-    const [products, setProducts] = useState<Product[]>(exampleProducts);
-    const [recommendedProducts, setRecommendedProducts] = useState<Product[]>(exampleProducts);
-    const [bestSellers, setBestSellers] = useState<Product[]>(exampleProducts);
-    const [surprises, setSurprises] = useState<Product[]>(exampleProducts);
-    const [coffeeEquipments, setCoffeeEquipments] = useState<Product[]>(exampleProducts);
-
-    const renderProductList = ({ data, title }: ProductListProps) => (
-        <Card title={title} style={{ width: '100%', marginTop: '20px' }}>
-            <Carousel arrows slidesToShow={5} slidesToScroll={5} infinite={false}>
-                {data.map(item => (
-                    <div key={item.id} style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-                        <Card style={{ width: 240 }}
-                              cover={<img src={item.image} alt={item.name} style={{ padding: '10px' }} />}
-                        >
-                            <strong>{item.name}</strong>
-                            <p>Price: ${item.price}</p>
-                            <Button icon={<ShoppingCartOutlined />} type="primary" style={{ width: '100%' }} onClick={() => {
-                                messageApi.success("Added to Cart Successfully");
-                            }}>
-                                Add to Cart
-                            </Button>
-                        </Card>
-                    </div>
-                ))}
-            </Carousel>
-        </Card>
-    );
-
+export default function Shop(){
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
-            {contextHolder}
-            {renderProductList({ data: products, title: 'Products' })}
-            {renderProductList({ data: recommendedProducts, title: 'Recommended Products' })}
-            {renderProductList({ data: bestSellers, title: 'Best Sellers' })}
-            {renderProductList({ data: surprises, title: 'Surprise Me' })}
-            {renderProductList({ data: coffeeEquipments, title: 'Coffee Equipment' })}
+        <div>
+            <ShopClient/>
         </div>
     );
-};
-
-export default ProductPage;
+}
---
+++ frontend/src/components/ShopClient.tsx
@@ -0,0 +1,150 @@
+'use client';
+import React, {useEffect, useState} from 'react';
+import {Button, Card, Empty, Input, Select, Space, Spin} from 'antd';
+import {useRouter, useSearchParams} from 'next/navigation'
+import ShopCategory from "@/components/ShopCategory";
+import axios from "axios";
+import {baseURL} from "@/lib/const";
+import {ProductDto} from "@/lib/entity/product";
+import ShopCategoryRecommendedProducts from "@/components/ShopCategoryRecommendedProducts";
+import {nameFormatter} from "@/lib/utils";
+
+const splitCategories = (data: ProductDto[]): {name: string, products: ProductDto[]}[] => {
+    const categories: {name: string, products: ProductDto[]}[] = [];
+        data.forEach(product => {
+            const category = categories.find(category => category.name === product.productCategoryName);
+            if (category) {
+                category.products.push(product);
+            } else {
+                categories.push({name: product.productCategoryName, products: [product]});
+            }
+        });
+        return categories;
+}
+
+const ShopClient: React.FC = () => {
+    const searchParams = useSearchParams();
+    const [data, setData] = useState<{name: string, products: ProductDto[]}[]>();
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+    const [searchInput, setSearchInput] = useState<string >("");
+
+    const router = useRouter();
+    const search = searchParams.get('search');
+    const category = searchParams.get('category');
+
+    useEffect(() => {
+        const search = searchParams.get('search');
+        const category = searchParams.get('category');
+
+        const fetchProducts = async () => {
+            setLoading(true);
+            console.log(search, category)
+            const queryParams = new URLSearchParams();
+
+            if(search && search.length > 0) {
+                queryParams.append('searchKey', search);
+            }
+
+            if(category) {
+                queryParams.append('category', category);
+            }
+
+            try {
+                const response = await axios.get(`${baseURL}/products/get?${queryParams.toString()}`);
+                if (response.status === 200) {
+                    setData(splitCategories(response.data as ProductDto[]));
+                }
+                else {
+                    setError("Failed to fetch products");
+                }
+            }
+            catch (e) {
+                setError("Failed to fetch products");
+                console.error(e)
+            }
+            setLoading(false);
+        }
+
+        fetchProducts();
+    }, [search, category]);
+
+    if(loading) {
+        return <Spin fullscreen/>
+    }
+
+    if(error) {
+        return <div>{error}</div>
+    }
+
+    if(!data || data.length === 0) {
+
+        return <Card> <Empty/> </Card>
+    }
+
+    function handleSearchButton() {
+        const queryParams = new URLSearchParams();
+
+        if(searchInput && searchInput.length > 0) {
+            queryParams.append('search', searchInput);
+        }
+
+        if(selectedCategory) {
+            queryParams.append('category', selectedCategory);
+        };
+
+        const url = `/shop?${queryParams.toString()}`;
+
+        router.push(url);
+
+    }
+
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
+            <Card>
+                <Space>
+                    <Input value={searchInput} onChange={(e) => setSearchInput(e.target.value)}/>
+                    <Select
+                        showSearch
+                        placeholder={"Select Category"}
+                        style={{ width: 200 }}
+                        allowClear
+                        options={data.map(category => ({label: nameFormatter(category.name), value: category.name}))}
+                        value={selectedCategory}
+                        onChange={(value) => {setSelectedCategory(value)}}
+                    />
+                    <Button type={"primary"} onClick={handleSearchButton} disabled={selectedCategory==null && searchInput.length == 0}>Search</Button>
+                </Space>
+
+            </Card>
+            {(!search && !category) && <ShopCategoryRecommendedProducts/>}
+            { data?.map(categoryItem => {
+                if(category) {
+                    if(categoryItem.name !== category) {
+                        return null;
+                    }
+                }
+                else {
+                    return <ShopCategory
+                        key={categoryItem.name}
+                        title={nameFormatter(categoryItem.name)}
+                        data={categoryItem.products}
+                        categoryName={categoryItem.name}
+                        full={false}
+                    />
+                }
+                
+                return <ShopCategory
+                    key={categoryItem.name}
+                    title={nameFormatter(categoryItem.name)}
+                    data={categoryItem.products}
+                    categoryName={categoryItem.name}
+                    full={true}
+                />
+            })}
+        </div>
+    );
+};
+
+export default ShopClient;
