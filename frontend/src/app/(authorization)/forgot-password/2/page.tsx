0b7d9ff5bbccc67c85de92fb0cbaf4c5a6cf37a3
Muhammed Tahir Akdeniz <b2200356027@cs.hacettepe.edu.tr>
2024-05-14 20:45:56 +0300

    forgot-password completed

--- frontend/src/app/(authorization)/forgot-password/2/page.tsx
+++ frontend/src/app/(authorization)/forgot-password/2/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { Form, Input, Button } from 'antd';
+import {Form, Input, Button, message} from 'antd';
 import { MailOutlined, LockOutlined } from '@ant-design/icons';
 import { useRouter } from 'next/navigation';
 import Image from "next/image";
@@ -9,27 +9,59 @@ import Coffee from "@/assets/images/coffee1.svg";
 import { Typography } from "antd";
 import ImageSide from "@/components/ImageSide";
 import cafeImage from "@/assets/images/cafe.png";
+import axios from "axios";
+import {baseURL} from "@/lib/const";
 const { Title, Text } = Typography;
 
 // Define an interface for the form data
 interface ForgotPasswordFormData {
-    code: string;
-    newPassword: string;
+    otp: string;
+    password: string;
     confirmPassword: string;
 }
 
 export default function ForgotPassword() {
     const router = useRouter();
+    const [loading, setLoading] = React.useState<boolean>(false);
+    const [messageApi, contextHolder] = message.useMessage();
 
-    const onFinish = ({ code, newPassword, confirmPassword }: ForgotPasswordFormData) => {
-        // Logic to handle password reset request here
-        console.log('Password reset process initiated with code:', code);
-        // Optionally redirect user or display a confirmation message
+    const onFinish = async ({ otp, password, confirmPassword }: ForgotPasswordFormData) => {
+        console.log({
+            username: localStorage.getItem("FORGOT_PASSWORD_USERNAME"),
+            password: password,
+            otp: otp
+        })
+        setLoading(true);
+        try {
+            const response = await axios.put(`${baseURL}/password-reset`, {
+                username: localStorage.getItem("FORGOT_PASSWORD_USERNAME"),
+                password: password,
+                otp: otp
+            });
+
+            if(response.status === 200){
+                messageApi.success("Password reset link sent to your email.");
+                router.push('/login');
+            }
+            else {
+                messageApi.error("Failed to send password reset link.");
+                console.error('An error occurred:', response.data.message || response.statusText);
+            }
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                messageApi.error(error.response?.data.message || error.message);
+                console.error('An error occurred:', error.response?.data.message || error.message);
+            }
+            else{
+                messageApi.error(`Failed to send password reset link.`);
+                console.error('An error occurred:', error);
+            }
+        }
+        setLoading(false);
     };
 
     return (
         <div className="flex flex-wrap">
-            <ImageSide imageSrc={cafeImage.src} imageAlt="Coffee Shop" isImageOnRight={false}/>
             <div className="w-full max-w-md">
                 <div className="text-center mb-8 my-4">
                     <Image src={Coffee.src} alt="Login" width={100} height={100}/>
@@ -44,30 +76,31 @@ export default function ForgotPassword() {
                     onFinish={onFinish}
                 >
                     <Form.Item
-                        name="code"
+                        name="otp"
                         rules={[
                             { required: true, message: 'Please input the code sent to your email!' }
                         ]}
+                        style={{width: "100%"}}
                     >
-                        <Input prefix={<MailOutlined className="site-form-item-icon" />} placeholder="Code"/>
+                        <Input.OTP length={6} style={{width: '100%'}}/>
                     </Form.Item>
                     <Form.Item
-                        name="newPassword"
+                        name="password"
                         rules={[
                             { required: true, message: 'Please input your new password!' },
-                            { min: 6, message: 'Password must be at least 6 characters.' }
+                            { min: 8, message: 'Password must be at least 8 characters.' }
                         ]}
                     >
-                        <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} placeholder="New Password"/>
+                        <Input.Password prefix={<LockOutlined className="site-form-item-icon"/>} placeholder="New Password"/>
                     </Form.Item>
                     <Form.Item
                         name="confirmPassword"
-                        dependencies={['newPassword']}
+                        dependencies={['password']}
                         rules={[
                             { required: true, message: 'Please confirm your new password!' },
                             ({ getFieldValue }) => ({
                                 validator(_, value) {
-                                    if (!value || getFieldValue('newPassword') === value) {
+                                    if (!value || getFieldValue('password') === value) {
                                         return Promise.resolve();
                                     }
                                     return Promise.reject(new Error('The two passwords that you entered do not match!'));
@@ -78,7 +111,7 @@ export default function ForgotPassword() {
                         <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} placeholder="Confirm New Password"/>
                     </Form.Item>
                     <Form.Item>
-                        <Button type="primary" htmlType="submit" className="reset-password-form-button" block>
+                        <Button type="primary" htmlType="submit" className="reset-password-form-button" block loading={loading} disabled={loading}>
                             Change Password
                         </Button>
                     </Form.Item>
