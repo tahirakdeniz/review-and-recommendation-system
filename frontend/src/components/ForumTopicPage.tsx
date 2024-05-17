148474e08200e8882b186cad31fd5387dc135e50
Meltem Özen <meltemozen53@gmail.com>
2024-05-17 21:15:42 +0300

    Pagination added to the forum

--- frontend/src/components/ForumTopicPage.tsx
+++ frontend/src/components/ForumTopicPage.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Avatar, Button, Card, Col, Form, Input, Modal, Row, Space, message } from "antd";
+import { Avatar, Button, Card, Col, Form, Input, Modal, Pagination, Row, Space, message } from "antd";
 import { CloseCircleOutlined } from '@ant-design/icons';
 import Meta from "antd/es/card/Meta";
 import TextArea from "antd/es/input/TextArea";
@@ -37,22 +37,34 @@ const AddNewMessageModal = ({isModalOpen, setIsModalOpen}) => {
 const ForumTopicPage: React.FC<ForumTopicPageProps> = ({topicId}) => {
     
     const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const showModal = () => {
-      setIsModalOpen(true);
-    };
+    const [currentPage, setCurrentPage] = useState(1);
     
-
     const messages = [
         { id: '1', user: "user1", content: 'Content of message 1 ',  date: '2024-05-12' },
-        { id: '2', user: "user2", content: 'Content of message 2',  date: '2024-05-11' }
+        { id: '2', user: "user2", content: 'Content of message 2',  date: '2024-05-11' },
+        { id: '3', user: "user3", content: 'Content of message 3 ',  date: '2024-05-12' },
+        { id: '4', user: "user4", content: 'Content of message 4',  date: '2024-05-11' }
     ];
     const topicTitle = "Today is my birthday!!"
+
+    const pageSize = 2;
+
+    const showModal = () => {
+        setIsModalOpen(true);
+      };
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const paginatedMessages = messages.slice(startIndex, endIndex);
 
  return(
         <div>
         <Card title={topicTitle}>
-            {messages.map(message => (
+            {paginatedMessages.map(message => (
                     <Card key={message.id} style={{ marginBottom: 12, minHeight: 60 }} >
                         <Button 
                             style={{ position: 'absolute', top: 8, right: 8, backgroundColor: 'transparent', border: 'none' }} 
@@ -82,6 +94,14 @@ const ForumTopicPage: React.FC<ForumTopicPageProps> = ({topicId}) => {
             ))}
             <div style={{ position: 'relative', left: 16 }}>
                 <Button type="primary" onClick={()=>{showModal()}}>Write a Message</Button>
+            </div>
+            <div style={{ position: 'absolute', right: 16, bottom: 16}}>
+                <Pagination
+                    current={currentPage}
+                    pageSize={pageSize}
+                    total={messages.length}
+                    onChange={handlePageChange}
+                />
             </div>
         </Card>
         <AddNewMessageModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
