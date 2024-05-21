4930f700a6b079d151ff3fcf72c9c50f00d0a76e
Muhammed Tahir Akdeniz <b2200356027@cs.hacettepe.edu.tr>
2024-05-21 15:54:19 +0300

    product page card width problem solved

--- frontend/src/components/ProductPage.tsx
+++ frontend/src/components/ProductPage.tsx
@@ -13,11 +13,11 @@ import {
     Modal,
     Rate,
     Row,
-    Space,
+    Space, Spin,
     Typography
 } from "antd";
-import {HeartOutlined, ShoppingCartOutlined, StarOutlined} from '@ant-design/icons';
-import {useEffect, useState} from "react";
+import {HeartOutlined, LoadingOutlined, ShoppingCartOutlined, StarOutlined} from '@ant-design/icons';
+import React, {useEffect, useState} from "react";
 import {ProductDto, ProductReviewReviewDto} from "@/lib/dto";
 import {useRole} from "@/lib/useRole";
 import {useImmer} from "use-immer";
@@ -29,6 +29,7 @@ import {addProductToCart} from "@/lib/redux/features/cart/cartSlice";
 import {useSelector} from "react-redux";
 import axios from "axios";
 import ProductImageView from "@/components/ProductImageView";
+import {useProductImage} from "@/lib/useProductImage";
 
 const {Text, Title} = Typography;
 
@@ -43,6 +44,7 @@ const ProductPage: React.FC<ProductPageProps> = ({product}) => {
     const [selectedReview, setSelectedReview] = useState<ProductReviewReviewDto | null>(null);
     const dispatch = useDispatch();
     const {loading: addToCartLoading, error: addToCartError} = useSelector((state: RootState) => state.cart);
+    const {image, loading, error, noImage} = useProductImage(product.id);
 
     const {isAuthorized: isMerchant} = useRole({role: 'MERCHANT'});
     const {isAuthorized: isAdmin} = useRole({role: 'ADMIN'});
@@ -156,17 +158,26 @@ const ProductPage: React.FC<ProductPageProps> = ({product}) => {
                 />
             )}
             <Flex style={{padding: '20px'}}>
-                <Space direction={'vertical'} size={20}>
+                <Space direction={'vertical'} size={20} style={{width: '100%'}}>
                     <Card
                         title={<Title level={1}>{productState.name}</Title>}
                         extra={<Typography.Link
                             href={`/shop?category=${productState.productCategoryName}`}>#{productState.productCategoryName}</Typography.Link>}
+                        style={{width: '100%'}}
                     >
                         <Row gutter={[16, 16]} justify="center" align="top">
                             <Col xs={24} lg={12}>
-                                <div style={{width: '100%', height: '100%', overflow: 'hidden'}}>
-                                    <ProductImageView productId={product.id} />
-                                </div>
+                                <Flex align={'center'} justify={'center'} style={{overflow: "hidden"}}>
+                                    {loading ? (
+                                        <Spin indicator={<LoadingOutlined style={{fontSize: 24}} spin/>}/>
+                                    ) : (
+                                        <Image
+                                            src={image || noImage}
+                                            alt={productState.name}
+                                            height={300}
+                                        />
+                                    )}
+                                </Flex>
                             </Col>
                             <Col xs={24} lg={12}>
                                 <div>
