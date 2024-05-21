4bfedac3d4f132112bd41913996e796eb945388c
Muhammed Tahir Akdeniz <b2200356027@cs.hacettepe.edu.tr>
2024-05-21 22:39:41 +0300

    admin review forms done

--- frontend/src/components/AdministrationReviewFormSection.tsx
+++ frontend/src/components/AdministrationReviewFormSection.tsx
@@ -1,23 +1,59 @@
-import React, {useState} from "react";
-import {Button, Card, Col, Form, Input, Modal, Row, Typography} from "antd";
-import {reviewFormDtosExample} from "@/lib/examples/reviewFormDtosExample";
+import React, { useState } from "react";
+import { Button, Card, Col, Form, Input, Modal, Row, Typography, message, Spin, Select } from "antd";
+import { reviewFormDtosExample } from "@/lib/examples/reviewFormDtosExample";
 import AdministrationReviewForm from "@/components/AdministrationReviewForm";
 import "@/styles/ReviewFields.css";
-import {ReviewFormDto} from "@/lib/dto";
-import {useImmer} from "use-immer";
+import {ProductCategoryDto, ReviewFormDto} from "@/lib/dto";
+import { useImmer } from "use-immer";
+import axios, { AxiosError } from 'axios';
+import { errorHandler } from "@/lib/utils";
+import {baseURL} from "@/lib/const"; // Ensure this path is correct
 
 export const AdministrationReviewFormSection = () => {
     const [reviewFormDtos, setReviewFormDtos] = useImmer<ReviewFormDto[]>([]);
     const [isAddModalVisible, setIsAddModalVisible] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [form] = Form.useForm();
+    const [messageApi, contextHolder] = message.useMessage();
+    const [productCategories, setProductCategories] = useState<ProductCategoryDto[]>([]);
+
 
     const getReviewForms = async () => {
-        // TODO implement API call later
-        setReviewFormDtos(reviewFormDtosExample);
+        setLoading(true);
+        try {
+            const response = await axios.get<ReviewFormDto[]>(`${baseURL}/reviews`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            });
+            setReviewFormDtos(response.data);
+            messageApi.success("Review forms fetched successfully");
+        } catch (error) {
+            const errorMessage = errorHandler(error, 'Get Review Forms');
+            messageApi.error(errorMessage);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+
+    const getProductCategories = async () => {
+        try {
+            const response = await axios.get<ProductCategoryDto[]>(`${baseURL}/product-categories/get`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            });
+            setProductCategories(response.data);
+        } catch (error) {
+            const errorMessage = errorHandler(error, 'Get Product Categories');
+            messageApi.error(errorMessage);
+        }
     };
 
     React.useEffect(() => {
         getReviewForms();
+        getProductCategories();
     }, []);
 
     const showAddModal = () => {
@@ -26,17 +62,34 @@ export const AdministrationReviewFormSection = () => {
     };
 
     const handleAddOk = () => {
-        // TODO implement API call to add new review form
-        form.validateFields().then(values => {
-            setReviewFormDtos(draft => {
-                draft.push({ // todo change this to the actual API call
-                    id: draft.length + 1, // This is a simple way to generate a new id
-                    productCategoryDto: { id: 0, name: values.productCategory , description: ""},
-                    reviewFieldDtos: [],
+        form.validateFields().then(async values => {
+            setLoading(true);
+            try {
+                const response = await axios.post(`${baseURL}/reviews`, {
+                    productCategoryName: values.productCategory,
+                }, {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                    }
                 });
-            });
-            setIsAddModalVisible(false);
-            form.resetFields();
+
+                setReviewFormDtos(draft => {
+                    draft.push({
+                        id: response.data.id,
+                        productCategoryDto: { id: response.data.productCategoryDto.id, name: values.productCategory , description: ""},
+                        reviewFieldDtos: [],
+                    });
+                });
+                messageApi.success("Review form added successfully");
+                setIsAddModalVisible(false);
+                form.resetFields();
+            } catch (error) {
+                const errorMessage = errorHandler(error, 'Add Review Form');
+                messageApi.error(errorMessage);
+            } finally {
+                setLoading(false);
+                getReviewForms();
+            }
         }).catch(info => {
             console.log('Validate Failed:', info);
         });
@@ -46,33 +99,51 @@ export const AdministrationReviewFormSection = () => {
         setIsAddModalVisible(false);
     };
 
-    const deleteReviewForm = (id: number) => {
+    const deleteReviewForm = async (id: number) => {
         Modal.confirm({
             title: 'Delete Field',
             content: 'Are you sure you want to delete this review form?',
-            onOk() {
-                setReviewFormDtos(draft => {
-                    return draft.filter(form => form.id !== id);
-                });
+            onOk: async () => {
+                setLoading(true);
+                try {
+                    await axios.delete(`${baseURL}/reviews/form/${id}`, {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                        }
+                    });
+
+                    setReviewFormDtos(draft => {
+                        return draft.filter(form => form.id !== id);
+                    });
+                    messageApi.success("Review form deleted successfully");
+                } catch (error) {
+                    const errorMessage = errorHandler(error, 'Delete Review Form');
+                    messageApi.error(errorMessage);
+                } finally {
+                    setLoading(false);
+                }
             },
         });
     };
 
     return (
         <>
+            {contextHolder}
             <Card
                 title={<Typography.Title level={3}>Review Forms</Typography.Title>}
                 extra={<Button type="primary" onClick={showAddModal}>Add New Form</Button>}
             >
-                <Row gutter={16}>
-                    {reviewFormDtos.map((form, index) => (
-                        <Col key={index} span={5}>
-                            <AdministrationReviewForm data={form} onChange={(value) => {
-                                console.log(JSON.stringify(value));
-                            }} onDelete={() => deleteReviewForm(form.id)} />
-                        </Col>
-                    ))}
-                </Row>
+                <Spin spinning={loading}>
+                    <Row gutter={16}>
+                        {Array.isArray(reviewFormDtos) && reviewFormDtos.map((form, index) => (
+                            <Col key={index} span={5}>
+                                <AdministrationReviewForm data={form} onChange={(value) => {
+                                    console.log(JSON.stringify(value));
+                                }} onDelete={() => deleteReviewForm(form.id)} fetchReviewForms={getReviewForms}/>
+                            </Col>
+                        ))}
+                    </Row>
+                </Spin>
             </Card>
 
             <Modal
@@ -80,6 +151,7 @@ export const AdministrationReviewFormSection = () => {
                 open={isAddModalVisible}
                 onOk={handleAddOk}
                 onCancel={handleAddCancel}
+                confirmLoading={loading}
             >
                 <Form form={form} layout="vertical">
                     <Form.Item
@@ -87,10 +159,17 @@ export const AdministrationReviewFormSection = () => {
                         label="Product Category"
                         rules={[{ required: true, message: 'Please input the product category!' }]}
                     >
-                        <Input />
+                        <Select
+                            placeholder="Select a product category"
+                            options={productCategories.map(category => ({
+                                label: category.name,
+                                value: category.name,
+                            }))}
+                        />
                     </Form.Item>
                 </Form>
             </Modal>
+
         </>
     );
 };
