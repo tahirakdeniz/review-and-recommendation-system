92ea33bb8f1ec8bf7df35c300e99659627e02703
Muhammed Tahir Akdeniz <b2200356027@cs.hacettepe.edu.tr>
2024-05-22 22:06:46 +0300

    Added RecommendedProducts

--- frontend/src/components/ShopCategoryRecommendedProducts.tsx
+++ frontend/src/components/ShopCategoryRecommendedProducts.tsx
@@ -1,24 +1,32 @@
 import ShopCategory from "@/components/ShopCategory";
 import {useEffect, useState} from "react";
 import {ProductDto} from "@/lib/dto";
+import axios from "axios";
+import {baseURL} from "@/lib/const";
+import {errorHandler} from "@/lib/utils";
 
 export default function ShopCategoryRecommendedProducts(){
     const [data, setData] = useState<ProductDto[]>([]);
+    const [error, setError] = useState<string | null>(null);
     const fetchRecommendedProducts = async () => {
-        // TODO implement this
-        // try {
-        //     const response = await axios.get(`${baseURL}/products/getRecommended`);
-        //     if (response.status !== 200) {
-        //         setError("Failed to fetch recommended products");
-        //     }
-        //     else {
-        //         setRecommendedProducts(response.data as ProductDto[]);
-        //     }
-        // }
-        // catch (e) {
-        //     setError("Failed to fetch recommended products");
-        //     console.error(e)
-        // }
+        try {
+            const response = await axios.get(`${baseURL}/recommendations/get`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            });
+            if (response.status !== 200) {
+                setError("Failed to fetch recommended products");
+            }
+            else {
+                setData(response.data as ProductDto[]);
+            }
+        }
+        catch (e) {
+            setError(errorHandler(e, "Recommended products"));
+            console.error(e)
+        }
     }
 
     useEffect(() => {
