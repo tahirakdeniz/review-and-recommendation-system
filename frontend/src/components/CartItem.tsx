0d935538a408f0cb7ac116cbb1171e4d8c8c5364
Muhammed Tahir Akdeniz <b2200356027@cs.hacettepe.edu.tr>
2024-05-24 01:53:37 +0300

    Wishlist Fixed

--- frontend/src/components/CartItem.tsx
+++ frontend/src/components/CartItem.tsx
@@ -1,10 +1,10 @@
-import { Badge, Button, Card, message, Rate, Skeleton, Tooltip } from 'antd';
-import { DeleteOutlined, PlusOutlined } from '@ant-design/icons';
-import { useDispatch } from "@/lib/redux/store";
-import { addProductToCart, removeProduct } from "@/lib/redux/features/cart/cartSlice";
-import { useRouter } from 'next/navigation';
-import { useProductImage } from "@/lib/useProductImage";
-import styled from 'styled-components';
+import {Card, Image, message, Skeleton, Tooltip} from 'antd';
+import {DeleteOutlined, PlusOutlined} from '@ant-design/icons';
+import {useDispatch} from "@/lib/redux/store";
+import {addProductToCart, removeProduct} from "@/lib/redux/features/cart/cartSlice";
+import {useRouter} from 'next/navigation';
+import {useProductImage} from "@/lib/useProductImage";
+import {ImageContainer, StyledButton, StyledCard} from "@/components/StyledCard";
 
 interface CartItemProps {
     name: string;
@@ -13,30 +13,6 @@ interface CartItemProps {
     count: number;
     id: number;
 }
-
-const StyledCard = styled(Card)`
-    width: 100%;
-    text-align: center;
-    border-radius: 10px;
-    overflow: hidden;
-    transition: transform 0.3s, box-shadow 0.3s;
-    &:hover {
-        transform: scale(1.03);
-        box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
-    }
-`;
-
-const StyledButton = styled(Button)`
-    margin: 0 5px;
-`;
-
-const ImageContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 200px;
-    overflow: hidden;
-`;
 
 export default function CartItem({ name, rating, price, count, id }: CartItemProps) {
     const dispatch = useDispatch();
@@ -55,12 +31,12 @@ export default function CartItem({ name, rating, price, count, id }: CartItemPro
                 cover={
                     <ImageContainer>
                         {loading ? <Skeleton.Image style={{ width: '100%', height: '100%' }} /> :
-                            <img alt={name} src={image || noImage} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                            <Image alt={name} src={image || noImage} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
                         }
                     </ImageContainer>
                 }
                 actions={[
-                    <Tooltip title="Delete">
+                    <Tooltip title="Delete" key={'delete'}>
                         <StyledButton key='delete' type="primary" danger icon={<DeleteOutlined />} size="small" onClick={async (e) => {
                             stopPropagation(e);
                             if (id) {
@@ -71,7 +47,7 @@ export default function CartItem({ name, rating, price, count, id }: CartItemPro
                             }
                         }} />
                     </Tooltip>,
-                    <Tooltip title="Add">
+                    <Tooltip title="Add" key={'add'}>
                         <StyledButton key='add' type="primary" icon={<PlusOutlined />} size="small" onClick={async (e) => {
                             stopPropagation(e);
                             if (id) {
